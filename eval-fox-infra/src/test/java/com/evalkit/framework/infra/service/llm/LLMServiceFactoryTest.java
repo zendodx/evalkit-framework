dd33b2b4cfed4896bf86910123baf3ce498461b5
zendodx <zendodx@hotmail.com>
2025-09-29 11:53:29 +0800

    fix: 单元测试读取配置中的密钥

--- eval-fox-infra/src/test/java/com/evalkit/framework/infra/service/llm/LLMServiceFactoryTest.java
+++ eval-fox-infra/src/test/java/com/evalkit/framework/infra/service/llm/LLMServiceFactoryTest.java
@@ -1,5 +1,6 @@
 package com.evalkit.framework.infra.service.llm;
 
+import com.evalkit.framework.common.utils.runtime.RuntimeEnvUtils;
 import com.evalkit.framework.infra.service.llm.config.DeepseekLLMServiceConfig;
 import com.evalkit.framework.infra.service.llm.config.LLMServiceConfig;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +8,9 @@ import org.junit.jupiter.api.Test;
 
 @Slf4j
 class LLMServiceFactoryTest {
+
+    String deepSeekToken = RuntimeEnvUtils.getPropertyFromResource("secret.properties", "deepseek-token");
+
     @Test
     public void test() {
         // 注册DeepSeek_Test大模型服务
@@ -19,7 +23,7 @@ class LLMServiceFactoryTest {
 
         // 创建服务实例
         DeepseekLLMServiceConfig config = DeepseekLLMServiceConfig.builder()
-                .apiToken("xxx")
+                .apiToken(deepSeekToken)
                 .build();
         LLMService llmService = LLMServiceFactory.createLLMService("DeepSeek_Test", config);
 
