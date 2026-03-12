d9f9bdbba3a0ef2b88abeb5f295c8db61c4c1d9f
zendodx <zendodx@hotmail.com>
2026-03-12 19:57:13 +0800

    fix: 优化html报告评估指标统计图表

--- evalkit-eval/src/test/java/com/evalkit/framework/eval/core/CoreTest.java
+++ evalkit-eval/src/test/java/com/evalkit/framework/eval/core/CoreTest.java
@@ -208,7 +208,7 @@ public class CoreTest {
         };
 
         String fileName = "核心链路测试_" + DateUtils.nowToString();
-        htmlReporter = new HtmlReporter(fileName, HtmlReportStyle.DEFAULT);
+        htmlReporter = new HtmlReporter(fileName, HtmlReportStyle.GITHUB);
         csvReporter = new CsvReporter(fileName);
         excelReporter = new ExcelReporter(fileName);
         jsonReporter = new JsonReporter(fileName);
