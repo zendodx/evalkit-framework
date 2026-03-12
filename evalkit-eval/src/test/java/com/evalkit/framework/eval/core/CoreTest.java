38587f43c32cfe3ed0f5a1a244c2e6ee80d749e5
zendodx <zendodx@hotmail.com>
2026-03-12 16:50:08 +0800

    fix: 重构html报告

--- evalkit-eval/src/test/java/com/evalkit/framework/eval/core/CoreTest.java
+++ evalkit-eval/src/test/java/com/evalkit/framework/eval/core/CoreTest.java
@@ -88,7 +88,7 @@ public class CoreTest {
             @Override
             public List<InputData> prepareDataList() {
                 List<InputData> inputDatas = new ArrayList<>();
-                for (int i = 0; i < 100; i++) {
+                for (int i = 0; i < 10; i++) {
                     inputDatas.add(new InputData(1L, JsonUtils.fromJson("{\t\"query\":\"hello, {{holiday}}\",\"type\":\"1\"}", new TypeReference<Map<String, Object>>() {
                     })));
                 }
